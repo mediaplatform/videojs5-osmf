e0179decd23fcdcfba07fc8d71f001974910edbc
vpowers <vic.powers@gmail.com>
2016-10-13 10:35:57 -0500

    update

--- src/js/videojs5-osmf-controller-es6.js
+++ src/js/videojs5-osmf-controller-es6.js
@@ -1,5 +1,7 @@
-import videojs from 'video.js';
+//import videojs from 'video.js';
+import videojs from './video.js/src/js/video.js';
 
+const Component = videojs.getComponent('Component');
 const Flash = videojs.getComponent('Flash');
 const Tech = videojs.getComponent('Tech');
 
@@ -21,8 +23,48 @@ Osmf.canPlaySource = function(src){
     return type in Osmf.formats ? 'maybe' : '';
 };
 
+// Create setters and getters for attributes
+const _api = Osmf.prototype;
+const _readWrite = 'rtmpConnection,rtmpStream,preload,defaultPlaybackRate,playbackRate,autoplay,loop,mediaGroup,controller,controls,volume,muted,defaultMuted'.split(',');
+const _readOnly = 'error,seeking,played,streamType,currentLevel,levels,networkState,readyState,initialTime,startOffsetTime,paused,ended,videoWidth,videoHeight'.split(',');
+
+function _createSetter(attr){
+    var attrUpper = attr.charAt(0).toUpperCase() + attr.slice(1);
+    _api['set'+attrUpper] = function(val){
+        return this.el_.vjs_setProperty(attr, val);
+    };
+}
+
+function _createGetter(attr) {
+    _api[attr] = function(){
+        return this.el_.vjs_getProperty(attr);
+    };
+}
+
+// Create getter and setters for all read/write attributes
+for (let i = 0; i < _readWrite.length; i++) {
+    _createGetter(_readWrite[i]);
+    _createSetter(_readWrite[i]);
+}
+
+// Create getters for read-only attributes
+for (let i = 0; i < _readOnly.length; i++) {
+    _createGetter(_readOnly[i]);
+}
+
+Osmf.prototype.paused = function(){
+    return this.el_.vjs_paused();
+};
+
+//Not sure this function is needed
+Osmf.prototype.streamStatus = function(){
+    return this.el_.streamStatus();
+};
+
 videojs.options.osmf = {};
-Tech.registerTech('Osmf', Osmf);
 videojs.options.techOrder.push('Osmf');
+
+Component.registerComponent('Flash', Flash);
+Tech.registerTech('Osmf', Osmf);
 
 export default Osmf;
